9eb859de0d424bcca6504a47ce1de154dddf0ef9
MUKANTWARI Françoise <f.mukantwari@alustudent.com>
2022-05-28 19:34:46 +0200

    Update files

--- server.js
+++ server.js
@@ -5,3 +5,7 @@ const app = express()
 // use the express-static middleware to connect to public
 app.use(express.static("public"))
 
+// listen for requests 
+app.listen(process.env.PORT || 8000, 
+    () => console.log("Server is running..."));
+

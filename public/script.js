2069a0d538d5d7717d480144b957fb92d2f37375
MUKANTWARI Françoise <f.mukantwari@alustudent.com>
2022-05-29 20:21:37 +0200

    Update files

--- public/script.js
+++ public/script.js
@@ -1,4 +1,44 @@
-let globalURL = window.location.origin.includes("heroku") ? "https://aviation-frequency.herokuapp.com" : "http://localhost:3000";
+// let globalURL = window.location.origin.includes("heroku") ? "https://aviation-frequency.herokuapp.com" : "http://localhost:3000";
+
+
+// async function initMap(){
+//   await fetch(`${globalURL}/api/map`)
+//   .then(data => data.json())
+//    .then(res => {
+//        const map = new google.maps.Map(document.getElementById("map"),{
+//            center: {lat:-1.85, lng:30},
+//            zoom: 10
+//        });
+       
+//   res.data.receivers.map(receiver => {
+//       console.log(receiver)
+
+//    const marker= new google.maps.Marker({
+//     position: new google.maps.LatLng(receiver.latitude, receiver.longitute),
+//         map: map,
+//         draggable:false,
+//         // title: "ken",
+//         // label:"k",
+//         animation: google.maps.Animation.DROP
+//     })
+
+    
+//   const  radius = receiver.radius; 
+
+//   const  circle = new google.maps.Circle({
+//     map: map,
+//     radius: 1000 * radius
+//   });
+//   circle.bindTo('center', marker, 'position');
+
+// })
+// });
+
+// }
+
+// google.maps.event.addDomListener(window, 'load', initMap);
+
+let globalURL = window.location.origin.includes("heroku") ? "https://aviation-frequency.herokuapp.com" : "https://aviation-frequency.herokuapp.com";
 
 
 async function initMap(){
@@ -17,8 +57,6 @@ async function initMap(){
     position: new google.maps.LatLng(receiver.latitude, receiver.longitute),
         map: map,
         draggable:false,
-        // title: "ken",
-        // label:"k",
         animation: google.maps.Animation.DROP
     })
 
